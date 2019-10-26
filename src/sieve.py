b85169bca8bcd358d87c4578f23e75544c532894
Ian Be <ian84be@gmail.com>
2019-10-26 15:54:05 -0500

    w

--- src/sieve.py
+++ src/sieve.py
@@ -6,8 +6,9 @@ def sieve(num: int) -> list:
 	# more effecient to max out at sqrt of num
 	sqr = int(num**0.5+1.5)
 	for i in range(2, sqr):
-		for j in range(i*i, num+1, i):
-			primes[j] = False
+		if primes[i]:
+			for j in range(i*i, num+1, i):
+				primes[j] = False
 	return [n for n, isPrime in enumerate(primes) if isPrime]
 
 # # myImplementation based on wikipedia pseudocode
